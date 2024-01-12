bca6ce2d60b4cc23e9ade3adbc04c50cc44e53ff
Timur Nagimov <nagimov_timur@bk.ru>
2024-01-12 16:43:23 +0300

    doxygen language changing

--- for_doka/html/search/searchdata.js
+++ for_doka/html/search/searchdata.js
@@ -16,9 +16,9 @@ var indexSectionNames =
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions"
+  0: "Указатель",
+  1: "Классы",
+  2: "Файлы",
+  3: "Функции"
 };
 
